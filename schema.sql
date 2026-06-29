d9beda5f5556a5907b9a89e574c8ce4af4ba0d4d
rkletr <git@merkletr.ee>
2026-06-29 19:43:26 -0400

    defaults

--- schema.sql
+++ schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE item(
 CREATE INDEX item_status ON item(status, type);
 
 CREATE TABLE linktype(type TEXT PRIMARY KEY NOT NULL, back TEXT NOT NULL, acyclic INTEGER NOT NULL) STRICT;
-INSERT INTO linktype (type, back) VALUES ('blocks', 'blocked-by'); -- a blocks b / b blocked by a
+INSERT INTO linktype (type, back, acyclic) VALUES ('blocks', 'blocked-by', 1);
+INSERT INTO linktype (type, back, acyclic) VALUES ('related-to', 'related-to', 0);
 
 CREATE TABLE link(
     type TEXT NOT NULL,
@@ -24,3 +25,16 @@ CREATE TABLE link(
     FOREIGN KEY (b) REFERENCES item (id)
 ) STRICT;
 CREATE INDEX backlinks ON link(b, type, a);
+
+CREATE TRIGGER check_no_cycles BEFORE INSERT ON link
+WHEN (SELECT acyclic FROM linktype WHERE type = NEW.type)
+BEGIN
+    SELECT RAISE(ABORT, 'cycle detected')
+    WHERE NEW.a = NEW.b OR EXISTS (
+        WITH RECURSIVE r AS (
+            SELECT b FROM link WHERE a = NEW.b AND type = NEW.type
+            UNION ALL
+            SELECT link.b FROM link JOIN r ON link.a = r.b WHERE link.type = NEW.type
+        ) SELECT 1 FROM r WHERE r.b = NEW.a LIMIT 1
+    );
+END;
